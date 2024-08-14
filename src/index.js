e7e5a58e95fce0f585221594d08c2e97315461f5
Гаевский Олег <162346575+FlaxidHair@users.noreply.github.com>
2024-08-14 11:06:14 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let brackets = bracketsConfig.join("").replace(/,/g, "");
+  let newArr = [];
+  for (let bracket of str) {
+    let index = brackets.indexOf(bracket);
+
+    if (index % 2 === 0) {
+      if (
+        bracket === brackets[index + 1] &&
+        newArr[newArr.length - 1] === index
+      ) {
+        newArr.pop();
+      } else if (
+        bracket === brackets[index + 1] &&
+        newArr[newArr.length - 1] !== index
+      ) {
+        newArr.push(index);
+      } else {
+        newArr.push(index);
+      }
+    } else {
+      if (newArr.pop() !== index - 1) {
+        return false;
+      }
+    }
+  }
+  return newArr.length === 0;
+};
